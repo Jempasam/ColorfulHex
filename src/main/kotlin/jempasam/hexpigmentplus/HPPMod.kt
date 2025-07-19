80ab475be51c16b6dc8351841a8e72845bffcb88
Jempasam <samueldemont83@gmail.com>
2025-07-19 17:03:49 +0200

    Creative tab item sort

--- src/main/kotlin/jempasam/hexpigmentplus/HPPMod.kt
+++ src/main/kotlin/jempasam/hexpigmentplus/HPPMod.kt
@@ -1,11 +1,11 @@
 package jempasam.hexpigmentplus
 
 import at.petrak.hexcasting.api.HexAPI
+import at.petrak.hexcasting.common.lib.HexItems
 import jempasam.hexpigmentplus.item.HPPItems
 import jempasam.hexpigmentplus.recipe.HPPRecipes
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
-import net.minecraft.item.ItemGroup
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
@@ -23,7 +23,7 @@ object HPPMod : ModInitializer {
 		HPPRecipes
 		ItemGroupEvents.modifyEntriesEvent(RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier(HexAPI.MOD_ID, "hexcasting"))).register{ event ->
 			val stacks = HPPItems.magicianHat.getStack()+HPPItems.magicianCloak.getStack()
-			event.addAfter({_->false}, stacks, ItemGroup.StackVisibility.PARENT_AND_SEARCH_TABS)
+			event.addAfter(HexItems.CREATIVE_UNLOCKER, stacks)
 		}
 	}
 
